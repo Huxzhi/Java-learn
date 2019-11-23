9858e0c2477ce4834619ca24133f4ac875cfc312
pai <1213791959@qq.com>
2019-11-23 13:03:16 +0800

    实验13

--- src/ccbupt/task13/Test02.java
+++ src/ccbupt/task13/Test02.java
@@ -11,6 +11,7 @@ package ccbupt.task13;
  */
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -46,8 +47,9 @@ public class Test02 {
             rateMap.put(movieId, ave);
         }
 
+
         List<Map.Entry<Long, Double>> entrys = new ArrayList<>(rateMap.entrySet());
-        //2：调用Collections.sort(list,comparator)方法把Entry-list排序
+//2：调用Collections.sort(list,comparator)方法把Entry-list排序
         Collections.sort(entrys, new Comparator<Map.Entry<Long, Double>>() {
             @Override
             public int compare(Map.Entry<Long, Double> o1, Map.Entry<Long, Double> o2) {
@@ -61,18 +63,47 @@ public class Test02 {
             }
         });
         //3：遍历排好序的Entry-list，可得到按顺序输出的结果
-        for (Map.Entry<Long, Double> entry : entrys) {
+        //前十个
+        System.out.println("平均评价前十");
+        for (Map.Entry<Long, Double> entry : entrys.subList(0, 10)) {
             System.out.println(entry.getKey() + ":" + entry.getValue());
         }
+        //所有
+        System.out.println("\n\n所有\n\n");
+        for (Map.Entry<Long, Double> longDoubleEntry : rateMap.entrySet()) {
+            System.out.println(longDoubleEntry.getKey() + ":" + longDoubleEntry.getValue());
+        }
 
-        // 输出
+
+        // 写到文件中
         String path = "src/ccbupt/task13/data/result.txt";
         file = new File(path);
         if (!file.exists()) {
             file.getParentFile().mkdirs();
         }
         file.createNewFile();
+        FileWriter fw = new FileWriter(file, false);
+        // true表示追加
+
+        List<Map.Entry<Long, Ratings>> ratingsEntry = new ArrayList<>(map.entrySet());
+        Collections.sort(ratingsEntry, new Comparator<Map.Entry<Long, Ratings>>() {
+            @Override
+            public int compare(Map.Entry<Long, Ratings> o1, Map.Entry<Long, Ratings> o2) {
+                return o2.getValue().num - o1.getValue().num;
+            }
+        });
+        fw.write("前十\n");
+        for (Map.Entry<Long, Ratings> longRatingsEntry : ratingsEntry.subList(0, 10)) {
+            fw.write(longRatingsEntry.getKey() + " : " + longRatingsEntry.getValue().num + "\n");
+        }
+        fw.write("所有\n");
+
+        for (Map.Entry<Long, Ratings> entry : map.entrySet()) {
+            fw.write(entry.getKey() + " : " + entry.getValue().num + "\n");
+        }
+
     }
+//2：调用Collections.sort(list,comparator)方法把Entry-list排序
 
 
     static class Ratings {
@@ -82,6 +113,14 @@ public class Test02 {
         public Ratings(double ratings, int num) {
             this.ratings = ratings;
             this.num = num;
+        }
+
+        @Override
+        public String toString() {
+            return "Ratings{" +
+                    "ratings=" + ratings +
+                    ", num=" + num +
+                    '}';
         }
     }
 }
